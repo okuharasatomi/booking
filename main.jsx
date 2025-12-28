d16d07a4d6714556ae87ab46e3370c68790de9f9
okuharasatomi <okuharasatomi@gmail.com>
2025-12-28 15:02:50 +0900

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// index.html内にある <div id="root"></div> を探して、Reactアプリを起動させます
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
